67341a83f6434232728528046728464dbafedce0
powerlord1990 <fatik911turbo1@gmail.com>
2019-09-23 21:18:25 +0300

    выполнение дз 2.2

---
+++ src/main/java/Burgers.java
@@ -0,0 +1,31 @@
+public class Burgers {
+    int buns;
+    int cheese;
+    int chickenCutlet;
+    int cucumbers;
+    int tomato;
+    int beefCutlet;
+    int salad;
+    int onion;
+    boolean ketchup;
+    boolean mayonnaise;
+    boolean mustard;
+
+    int price;
+
+    Burgers(int b, int ch, int cC, int c, int t, int bC, int s, int o, boolean k, boolean m, boolean mu, int p) {
+
+        buns = b;
+        cheese = ch;
+        chickenCutlet = cC;
+        cucumbers = c;
+        tomato = t;
+        beefCutlet = bC;
+        salad = s;
+        onion = o;
+        ketchup = k;
+        mayonnaise = m;
+        mustard = mu;
+        price = p;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Burgers cheeseburger = new Burgers(2, 1, 1, 1, 0, 0, 0, 0, false, false, true, 69);
+        Burgers chefburger = new Burgers(2, 0, 1, 1, 0, 0, 1, 1, false, false, true, 129);
+    }
+}
